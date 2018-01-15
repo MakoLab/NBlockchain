5a4d8b589321595a5d976f7afe3ea966cf3391e7
MAKOLAB-NET\mariusz.postol <mariusz.postol@makolab.com>
2018-01-15 10:04:29 +0100

    Java framework API - prepare .NET proxy #3 - Implemented CryptoJS using helper function to convert byte[] to hex string. - UT using ToUpper conversion

--- P2PProtocolUnitTest/CryptoJSUnitTest.cs
+++ P2PProtocolUnitTest/CryptoJSUnitTest.cs
@@ -9,7 +9,7 @@ namespace NBlockchain.P2PPrototocol.lUnitTest
     public void TestMethod1()
     {
       string _sha = NodeJSAPI.CryptoJS.SHA256("Zażółć gęślą jaźń");
-      Assert.AreEqual<string>("bc5348fd7c2dd8bbf411f0b9268265f7c2e0d31ebf314695882b8170c7e1e9d7", _sha);
+      Assert.AreEqual<string>("bc5348fd7c2dd8bbf411f0b9268265f7c2e0d31ebf314695882b8170c7e1e9d7".ToUpper(), _sha);
     }
   }
 }
--- P2PPrototocol/NodeJSAPI/CryptoJS.cs
+++ P2PPrototocol/NodeJSAPI/CryptoJS.cs
@@ -8,15 +8,29 @@ namespace NBlockchain.P2PPrototocol.NodeJSAPI
 
   internal static class CryptoJS
   {
-    internal static string SHA256(string inputStream)
+    internal static string SHA256(this string inputStream)
     {
       byte[] _inputStreamBytes = Encoding.UTF8.GetBytes(inputStream);
       using (SHA256 mySHA256 = SHA256Managed.Create())
       {
         byte[] hashValue = mySHA256.ComputeHash(_inputStreamBytes);
-        return BitConverter.ToString(hashValue);
+        return hashValue.ToHexString();
       }
     }
+    private static string ToHexString(this byte[] bytes)
+    {
+      char[] c = new char[bytes.Length * 2];
+      int b;
+      for (int i = 0; i < bytes.Length; i++)
+      {
+        b = bytes[i] >> 4;
+        c[i * 2] = (char)(55 + b + (((b - 10) >> 31) & -7));
+        b = bytes[i] & 0xF;
+        c[i * 2 + 1] = (char)(55 + b + (((b - 10) >> 31) & -7));
+      }
+      return new string(c);
+    }
+
   }
 
 }
